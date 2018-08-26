ea12ec7c631ff8547a360a9d3e08b59d671e9529
Maryna222 <mankomv77@gmail.com>
2018-08-26 22:50:29 +0300

    New feature in new branch

---
+++ src/main/java/Test_mmenu_f.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test_mmenu_f {
+    public static void main(String[] args){
+        WebDriver driver = getInitFirefoxDriver();
+
+        driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
+
+        /*WebElement email_field = newdriver.findElement(By.id("email"));
+        email_field.sendKeys("webinar.test@gmail.com");
+
+        WebElement pass_field = newdriver.findElement(By.id("passwd"));
+        pass_field.sendKeys("Xcg7299bnSmMuRLp9ITw");
+
+        WebElement button_click = newdriver.findElement(By.name("submitLogin"));
+        button_click.click();*/
+
+
+
+    }
+
+    public static WebDriver getInitFirefoxDriver(){
+        System.setProperty("webdriver.gecko.driver",Test_mmenu_f.class.getResource("geckodriver.exe").getPath());
+        return new FirefoxDriver();
+    }
+
+}
+
