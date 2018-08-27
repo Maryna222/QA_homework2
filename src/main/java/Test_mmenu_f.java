16f62c584e6db1c96623b39b23be193912cd599d
Maryna222 <mankomv77@gmail.com>
2018-08-27 21:27:25 +0300

    Some changes

--- src/main/java/Test_mmenu_f.java
+++ src/main/java/Test_mmenu_f.java
@@ -1,13 +1,19 @@
+package org.openqa.selenium.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Test_mmenu_f {
-    public static void main(String[] args){
+    public static void main (String[] args){
         WebDriver driver = getInitFirefoxDriver();
-        driver.get("http://google.com");
-        driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
+       // driver.get("http://google.com");
+        driver.manage().window().maximize();
+        //driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
+        driver.navigate().to("http://google.com");
+
+       //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+       // driver.get("https://mail.ukr.net/desktop/login");
 
         /*WebElement email_field = newdriver.findElement(By.id("email"));
         email_field.sendKeys("webinar.test@gmail.com");
@@ -17,8 +23,7 @@ public class Test_mmenu_f {
 
         WebElement button_click = newdriver.findElement(By.name("submitLogin"));
         button_click.click();*/
-
-
+        //System.out.println("Hello world");
 
     }
 
