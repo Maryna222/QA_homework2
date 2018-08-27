a54f5411a7d31f9eef28394703d9c977504abf1e
Maryna222 <mankomv77@gmail.com>
2018-08-27 23:18:41 +0300

    First case for login

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -6,27 +6,29 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Test1 {
     public static void main(String[] args){
         WebDriver driver = getInitFirefoxDriver();
+        LoginPage login = new LoginPage(driver);
+        login.login("webinar.test@gmail.com", "Xcg7299bnSmMuRLp9ITw" );
 
-        driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
+        /*////////////Скрипт Б. Проверка работоспособности главного меню Админ панели//////////
+        WebElement email_field1 = driver.findElement(By.id("email"));
+        email_field1.sendKeys("webinar.test@gmail.com");
 
-        WebElement email_field = driver.findElement(By.id("email"));
-        email_field.sendKeys("webinar.test@gmail.com");
+        WebElement pass_field1 = driver.findElement(By.id("passwd"));
+        pass_field1.sendKeys("Xcg7299bnSmMuRLp9ITw");
 
-        WebElement pass_field = driver.findElement(By.id("passwd"));
-        pass_field.sendKeys("Xcg7299bnSmMuRLp9ITw");
+        WebElement button_click1 = driver.findElement(By.name("submitLogin"));
+        button_click1.click();
 
-        WebElement button_click = driver.findElement(By.name("submitLogin"));
-        button_click.click();
+        WebElement menu_item = driver.findElement(By.id("tab-AdminDashboard"));
+        menu_item.click();
 
+        Thread.sleep(20);
+        WebElement find_title = driver.findElement(By.class("page-title"));
+        System.out.println(find_title);*/
 
-        WebElement admin_avatar = driver.findElement(By.id("employee_infos"));
-        admin_avatar.click();
-
-        WebElement logot_elem = driver.findElement(By.id("header_logout"));
-        logot_elem.click();
     }
 
-    public static WebDriver getInitFirefoxDriver(){
+    private static WebDriver getInitFirefoxDriver(){
         System.setProperty("webdriver.gecko.driver",Test1.class.getResource("geckodriver.exe").getPath());
         return new FirefoxDriver();
     }
